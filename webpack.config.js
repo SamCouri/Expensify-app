d8ab6d2a7be0b0dd017182096725a710c0f52a8c
Samir Khoury <skhoury@EQ-MAC-37.local>
2022-06-12 12:54:06 -0400

    set up dev dependencies and dist folder

--- webpack.config.js
+++ webpack.config.js
@@ -4,13 +4,11 @@ const ExtractTextPlugin = require('extract-text-webpack-plugin');
 module.exports = (env) => {
   const isProduction = env === 'production';
   const CSSExtract = new ExtractTextPlugin('styles.css');
-  
-  //console.log('env :', env);
-  
+
   return {
   entry : './src/app.js',
   output : {
-    path: path.join(__dirname, 'public'),
+    path: path.join(__dirname, 'public', 'dist'),
     filename: 'bundle.js'
   },
 
@@ -47,7 +45,8 @@ plugins: [
 devtool : isProduction ? 'source-map' : 'inline-source-map',
 devServer : {
   contentBase: path.join(__dirname, 'public'),
-  historyApiFallback: true
+  historyApiFallback: true,
+  publicPath: '/dist/'
 }
 };
 };
